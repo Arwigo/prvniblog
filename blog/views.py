9d53715543e12a3b975d02c120be0ea69c2eb31d
Arwigo <miroslav.stolpa@gmail.com>
2016-12-10 16:40:59 +0100

    blog

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# Create your views here.
